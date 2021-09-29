476ce3644f11fb26efbf0dac914de902e789fd61
jasonlee <jlee7060@gmail.com>
2021-09-29 03:11:06 -0400

    added main image and fetching tmdb api

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,8 +1,43 @@
-import React from "react";
-import { FaCode } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import { API_URL, API_KEY, IMAGE_URL } from "../../Config";
+import MainImage from "./Sections/MainImage";
 
 function LandingPage() {
-  return <></>;
+  const [Movies, setMovies] = useState();
+  const [MainMovie, setMainMovie] = useState(null);
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => {
+        setMovies([response.results]);
+        setMainMovie(response.results[0]);
+        console.log(response.results);
+        console.log(response.results[0]);
+      });
+  }, []);
+  return (
+    <div style={{ width: "100%", margin: "0" }}>
+      {/* {main img} */}
+      {MainMovie && (
+        <MainImage
+          image={`${IMAGE_URL}w1280${MainMovie.backdrop_path}`}
+          title={MainMovie.original_title}
+          text={MainMovie.overview}
+        />
+      )}
+
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <h2>Movies by latest</h2>
+        <hr />
+        {/* movie grid cards */}
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>More</button>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
